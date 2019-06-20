4ee601616e9e4e8676c7f9acf686b7cc0eaf0b5f
mar1011 <mariana.suarezdelcerro@davinci.edu.ar>
2019-06-20 17:46:29 -0300

    Customizando panel

--- resources/views/panel/merch/form.blade.php
+++ resources/views/panel/merch/form.blade.php
@@ -16,39 +16,39 @@
                 <form action="{{route("merch.update",$merch->id)}}" enctype="multipart/form-data" method="POST" class=" p-3">
                     @method("PUT")
                     @else
-                        <form action="{{ route("productos.store") }}" enctype="multipart/form-data" method="POST" class="bg-light p-3">
+                        <form action="{{ route("merch.store") }}" enctype="multipart/form-data" method="POST" class="bg-light p-3">
                     @endif
                     @csrf
                     <div class="form-group">
-                        <label class="frase2" for="nombre">Nombre</label>
+                        <label class="" for="nombre">Nombre</label>
                         <input type="text" name="nombre" id="nombre" class="form-control" placeholder="Ingrese el nombre del producto" value="{{isset($merch)? $merch->nombre : old('nombre')}}">
                     </div>
 
                     <div class="form-group">
-                        <label class="frase2" for="descripcion">Descripción</label>
+                        <label class="" for="descripcion">Descripción</label>
                         <textarea type="text" name="descripcion" id="descripcion" class="form-control" aria-describedby="help_descripcion" >{{isset($merch) ? $merch->descripcion : old("descripcion")}}</textarea>
                         <small id="help_descripcion" class="text-muted">Descripción breve del producto.</small>
                     </div>
 
                         <div class="form-group">
-                            <label class="frase2" for="nombre">Precio</label>
-                            <input type="number" name="precio" id="precio" class="form-control" placeholder="Ingrese el precio del producto" value="{{isset($merch)? $merch->precio : old('precio')}}">
+                            <label class="" for="precio">Precio</label>
+                            <input type="text" name="precio" id="precio" class="form-control" placeholder="Ingrese el precio del producto" value="{{isset($merch)? $merch->precio : old('precio')}}">
                         </div>
 
-                        <!--<div class="form-check">
-                            <label for="number">Stock</label>
+                       <div class="form-check">
+                            <label for="stock">Stock: </label>
                             <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+                                <input class="form-check-input" type="radio" name="stock" id="inlineRadio1" value="option1">
                                 <label class="form-check-label" for="inlineRadio1">Si</label>
                             </div>
                             <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+                                <input class="form-check-input" type="radio" name="stock" id="inlineRadio2" value="option2">
                                 <label class="form-check-label" for="inlineRadio2">No</label>
                             </div>
-                        </div> -->
+                        </div>
                     @isset($merch)
                     <div class="form-group">
-                        <label class="frase2" for="imagen">Imagen</label>
+                        <label class="" for="imagen">Imagen</label>
                         <img src="{{ $merch->imagen }}" alt="" width="120">
                     </div>
                     @endif
