3216cb4c9a8ff424bd579b087e42b88ff4710cb3
mar1011 <mariana.suarezdelcerro@davinci.edu.ar>
2019-08-01 20:13:35 -0300

    abm usuarios

--- resources/views/panel/user/form.blade.php
+++ resources/views/panel/user/form.blade.php
@@ -1,1 +1,58 @@
-<?php
+@extends("panel.template")
+
+@section("contenido")
+
+    <div class="row nov">
+        <div class="container-fluid">
+            <h4 class="titulo text-center display-4">Cargar Nuevo y Editar</h4>
+        </div>
+    </div>
+
+    <div class="container">
+
+
+
+        <div class="row">
+            <div class="col-6 offset-3">
+
+                    <form action="" enctype="multipart/form-data" method="POST" class=" p-3">
+
+                            <form action="" enctype="multipart/form-data" method="POST" class="bg-light p-3">
+
+                                <div class="form-group">
+                                    <label class="text-white" for="nombre">Nombre</label>
+                                    <input type="text" name="nombre" id="nombre" class="form-control" placeholder="Ingrese el nombre del Usuario" value="">
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="text-white"  for="mail">E-mail</label>
+                                    <input type="text" name="mail" id="mail" class="form-control" placeholder="Ingrese el E-mail" value="">
+                                </div>
+
+                                <div  class="form-group">
+                                    <label class="text-white" for="user">User</label>
+                                    <input type="text" name="user" id="user" class="form-control" placeholder="Ingrese el User" value="">
+                                </div>
+
+                                <div class="form-group">
+                                <label class="text-white">Perfil</label>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="1">
+                                    <label class="form-check-label text-white" for="inlineRadio1">1</label>
+                                </div>
+                                <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="2">
+                                    <label class="form-check-label text-white" for="inlineRadio2">2</label>
+                                </div>
+
+
+                                <button type="submit" class="btn btn-primary">Cargar</button>
+                            </form>
+            </div>
+        </div>
+
+    </div>
+
+
+@endsection
--- resources/views/panel/user/index.blade.php
+++ resources/views/panel/user/index.blade.php
@@ -2,6 +2,30 @@
 
 @section("contenido")
 
+    <div class="container">
+        <div class="row nov">
+            <div class="col-12">
+                <h2 class="titulo text-center display-4">Lista de Merch</h2>
+
+                <a href="{{ route("user.create") }}" class="btn btn-lg btn-primary">Cargar Nuevo</a>
+            </div>
+        </div>
+
+        <div class="row mt-2 mb-5">
+
+            <div class="col-12">
+                <div class="table-responsive">
+                    <table class="table mt-5 nov table-dark">
+                        <thead class="thead-light text-center">
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Mail</th>
+                            <th>User</th>
+                            <th>Perfil</th>
+
+
+                        </tr>
+                        </thead>
 
 
 @endsection
