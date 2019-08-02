99c9fced8ee054e571452fcb1b2d29b4bf0133e6
mar1011 <mariana.suarezdelcerro@davinci.edu.ar>
2019-08-02 02:19:39 -0300

    abm usuarios

--- resources/views/panel/user/form.blade.php
+++ resources/views/panel/user/form.blade.php
@@ -38,17 +38,18 @@
                                     <input type="text" name="mail" id="mail" class="form-control" placeholder="Ingrese el E-mail" value="{{isset($user)? $user->email : old('email')}}">
                                 </div>
 
+                             <!--
                                 <div class="form-group">
                                     <label class= "text-white" for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
                                     <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
-                                </div>
-
+                                </div>*/
+                                  -->
                                 <div  class="form-group">
                                     <label class="text-white" for="user">User</label>
                                     <input type="text" name="user" id="user" class="form-control" placeholder="Ingrese el User" value="{{isset($user)? $user->user : old('user')}}">
                                 </div>
-
-                                <div class="form-group">
+                             <!--
+                               <div class="form-group">
                                     <label class="text-white">Perfil</label>
                                 </div>
 
@@ -60,8 +61,8 @@
                                 <div class="form-check form-check-inline">
                                     <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="2">
                                     <label class="form-check-label text-white" for="inlineRadio2">2</label>
-                                </div>
-
+                                </div>*/
+                               -->
 
                                 <button type="submit" class="btn btn-primary">Cargar</button>
                             </form>
